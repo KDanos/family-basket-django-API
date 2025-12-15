56790dc874bba8004e9e5815b8b5cbc5c1456e77
Konstantin <konstantin_danos@yahoo.de>
2025-12-15 15:39:34 +0200

    added CORS middleware and customised authentication messages

--- family_basket/settings.py
+++ family_basket/settings.py
@@ -45,7 +45,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'baskets',
-    'items'
+    'items', 
+
+    # Third party
+    'corsheaders',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
@@ -60,6 +63,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,6 +71,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',  # Vite default
+    'http://localhost:5174',  # Your current port
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
 ]
 
 ROOT_URLCONF = 'family_basket.urls'
--- users/models.py
+++ users/models.py
@@ -4,8 +4,17 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    email = models.EmailField (unique = True)
-    username = models.CharField(max_length = 255,unique = True)
+    email = models.EmailField (
+        unique = True,
+        error_messages = {
+            'unique': 'A user with this email already exits.'
+        })
+    username = models.CharField(
+        max_length = 255,
+        unique = True,
+        error_messages ={
+            'unique': 'A user with this username already exists.'
+        })
     profile_image = models.URLField (blank = True, null = True) 
     connections = models.ManyToManyField('self', symmetrical = True, blank = True)
     
